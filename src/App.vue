200cd4f840b8921f897c3686dfc39b7c1010921c
brugk000 <52834090+brugk000@users.noreply.github.com>
2022-02-04 15:26:14 +0100

    main

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome Vue.js "/>
+    <HelloWorld msg="Welcome Kees"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
   name: 'App',
   components: {
